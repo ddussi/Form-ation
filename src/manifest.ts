dba6b024d1ea1c4a7b21542d86c10e155eb19692
ddussi <akfn0919@naver.com>
2025-08-25 01:41:47 +0900

    Chore: Vite 설정에 crx 플러그인 추가 및 매니페스트 파일 연결

---
+++ src/manifest.ts
@@ -0,0 +1,13 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+
+export default defineManifest({
+  manifest_version: 3,
+  name: 'Form-ation',
+  version: '0.0.0',
+  action: {
+    default_popup: 'index.html',
+  },
+  // 필요한 권한/스크립트/아이콘은 이후 단계에서 추가합니다.
+})
+
+
